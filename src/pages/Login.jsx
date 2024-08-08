c9f04bebc5d37d9b2abc32104b84a65cd631b3e9
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-08-08 08:21:14 +0800

    Updating JWT

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { Form, Button } from 'react-bootstrap'
 import Swal from 'sweetalert2'
 import { useNavigate } from 'react-router-dom'
 import { TokenContext } from '../App';
+import { jwtDecode } from 'jwt-decode';
 
 const Login = () => { 
 
